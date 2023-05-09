c61976a3dcc027380c6144848b04362bc341a83d
mnefedov <mikhail.nefedov@stud.htwk-leipzig.de>
2023-05-09 13:22:28 +0200

    init

---
+++ src/main/java/com/github/onetraintransferproblemgenerator/OneTrainTransferProblemGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.github.onetraintransferproblemgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTrainTransferProblemGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneTrainTransferProblemGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/onetraintransferproblemgenerator/OneTrainTransferProblemGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.onetraintransferproblemgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTrainTransferProblemGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
