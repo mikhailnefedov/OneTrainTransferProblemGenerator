17d012df72d6b3e58028b418123954d3ba30e0f6
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2023-05-10 13:35:24 +0200

    Add computeCarriagePosition

--- src/main/java/com/github/onetraintransferproblemgenerator/models/RailCarriage.java
+++ src/main/java/com/github/onetraintransferproblemgenerator/models/RailCarriage.java
@@ -6,4 +6,12 @@ import lombok.Data;
 public class RailCarriage {
     private int sequenceNumber;
     private int capacity;
+
+    public int computeCarriagePosition(int trainLength, int position, DirectionOfTravel travelDirection) {
+        if (travelDirection.equals(DirectionOfTravel.ascending)) {
+            return position + sequenceNumber - 1;
+        } else {
+            return position + trainLength - sequenceNumber;
+        }
+    }
 }
