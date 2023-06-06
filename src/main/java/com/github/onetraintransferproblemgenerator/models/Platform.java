f151a8bad4988b90fc637efe69fec96f5ecb1f20
mnefedov <mikhail.nefedov@stud.htwk-leipzig.de>
2023-06-06 10:13:03 +0200

    Remove opencsv

--- src/main/java/com/github/onetraintransferproblemgenerator/models/Platform.java
+++ src/main/java/com/github/onetraintransferproblemgenerator/models/Platform.java
@@ -1,14 +1,12 @@
 package com.github.onetraintransferproblemgenerator.models;
 
-import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class Platform {
-    @CsvBindByName
+
     private int id;
-    @CsvBindByName
     private int positionCount;
 }
