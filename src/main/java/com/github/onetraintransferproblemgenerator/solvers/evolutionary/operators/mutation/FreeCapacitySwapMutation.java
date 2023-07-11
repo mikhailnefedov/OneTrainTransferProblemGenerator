9e22a60648cff65eb5afab223c9965cf999cc821
mnefedov <mikhail.nefedov@stud.htwk-leipzig.de>
2023-07-11 17:54:12 +0200

    ImplementPassengerSwap

--- src/main/java/com/github/onetraintransferproblemgenerator/solvers/evolutionary/operators/mutation/FreeCapacitySwapMutation.java
+++ src/main/java/com/github/onetraintransferproblemgenerator/solvers/evolutionary/operators/mutation/FreeCapacitySwapMutation.java
@@ -4,9 +4,7 @@ import com.github.onetraintransferproblemgenerator.models.Passenger;
 import com.github.onetraintransferproblemgenerator.solvers.evolutionary.models.Individual;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 public class FreeCapacitySwapMutation {
 
@@ -17,7 +15,7 @@ public class FreeCapacitySwapMutation {
         if (individual.getPassengerRailCarriageMapping().keySet().size() > 0) {
             Passenger passenger = chooseRandomPassenger(individual);
 
-            List<? extends Swap> swaps = createSwapsWithFreeCapacity(individual, passenger);
+            List<? extends Swap> swaps = SwapCreator.createSwapsWithFreeCapacity(individual, passenger);
             if (swaps.size() > 0) {
                 Swap swap = swaps.get(random.nextInt(swaps.size()));
                 swap.makeSwap(individual, passenger);
@@ -29,31 +27,6 @@ public class FreeCapacitySwapMutation {
         List<Passenger> passengers = individual.getPassengerRailCarriageMapping().keySet().stream().toList();
 
         return passengers.get(random.nextInt(passengers.size()));
-    }
-
-    private List<FreeCapacitySwap> createSwapsWithFreeCapacity(Individual individual, Passenger passenger) {
-        int currentRailCarriageId = individual.getPassengerRailCarriageMapping().get(passenger);
-        return individual.railCarriagesWithCapacityForRide(passenger.getInStation(), passenger.getOutStation()).stream()
-                .filter(id -> !id.equals(currentRailCarriageId))
-                .map(FreeCapacitySwap::new)
-                .toList();
-    }
-
-    private void getPassengerSwaps(Individual individual, Passenger passenger) {
-        Map<Integer, List<Passenger>> possiblePassengersOfRailCarriage = getPossiblePassengerSwapPartners(individual, passenger);
-
-        for (Integer railCarriageId : possiblePassengersOfRailCarriage.keySet()) {
-            List<Passenger> passengers = possiblePassengersOfRailCarriage.get(railCarriageId);
-            //TODO: Make it possible to swap with passengers too
-        }
-    }
-
-    private Map<Integer, List<Passenger>> getPossiblePassengerSwapPartners(Individual individual, Passenger passenger) {
-        return individual.getPassengerRailCarriageMapping().keySet().stream()
-                .filter(p -> p.getInStation() >= passenger.getInStation() &&
-                        p.getOutStation() <= passenger.getOutStation() &&
-                        !p.equals(passenger))
-                .collect(Collectors.groupingBy(p -> individual.getPassengerRailCarriageMapping().get(p)));
     }
 
 }
--- src/main/java/com/github/onetraintransferproblemgenerator/solvers/evolutionary/operators/mutation/PassengerSwap.java
+++ src/main/java/com/github/onetraintransferproblemgenerator/solvers/evolutionary/operators/mutation/PassengerSwap.java
@@ -3,10 +3,51 @@ package com.github.onetraintransferproblemgenerator.solvers.evolutionary.operato
 import com.github.onetraintransferproblemgenerator.models.Passenger;
 import com.github.onetraintransferproblemgenerator.solvers.evolutionary.models.Individual;
 
+import java.util.List;
+
 class PassengerSwap extends Swap {
+
+    private int newRailCarriageId;
+    private List<Passenger> passengersToSwap;
+
+    public PassengerSwap(int newRailCarriageId, List<Passenger> passengers) {
+        this.newRailCarriageId = newRailCarriageId;
+        passengersToSwap = passengers;
+    }
 
     @Override
     void makeSwap(Individual individual, Passenger passenger) {
+        int oldRailCarriageId = individual.getPassengerRailCarriageMapping().get(passenger);
+        updatePassengerRailCarriageMapping(individual, passenger, oldRailCarriageId);
 
+        updatePassengersOfRailCarriageMapping(individual, passenger, oldRailCarriageId);
+    }
+
+    private void updatePassengerRailCarriageMapping(Individual individual, Passenger passenger, int oldRailCarriageId) {
+        individual.getPassengerRailCarriageMapping().remove(passenger);
+        passengersToSwap.forEach(p -> individual.getPassengerRailCarriageMapping().remove(p));
+
+        individual.getPassengerRailCarriageMapping().put(passenger, newRailCarriageId);
+        passengersToSwap.forEach(p -> individual.getPassengerRailCarriageMapping().put(p, oldRailCarriageId));
+    }
+
+    private void updatePassengersOfRailCarriageMapping(Individual individual, Passenger passenger, int oldRailCarriageId) {
+        removePassengerFromPassengersOfRailCarriageMapping(individual, passenger, oldRailCarriageId);
+        passengersToSwap.forEach(p -> removePassengerFromPassengersOfRailCarriageMapping(individual, passenger, newRailCarriageId));
+
+        addPassengerToPassengersOfRailCarriageMapping(individual, passenger, newRailCarriageId);
+        passengersToSwap.forEach(p -> addPassengerToPassengersOfRailCarriageMapping(individual, passenger, oldRailCarriageId));
+    }
+
+    private void removePassengerFromPassengersOfRailCarriageMapping(Individual individual, Passenger passenger, int railCarriageId) {
+        for (int stationId = passenger.getInStation(); stationId < passenger.getOutStation(); stationId++) {
+            individual.getPassengersOfRailCarriageOfInStation().get(stationId).get(railCarriageId).removePassenger(passenger);
+        }
+    }
+
+    private void addPassengerToPassengersOfRailCarriageMapping(Individual individual, Passenger passenger, int railCarriageId) {
+        for (int stationId = passenger.getInStation(); stationId < passenger.getOutStation(); stationId++) {
+            individual.getPassengersOfRailCarriageOfInStation().get(stationId).get(railCarriageId).addPassenger(passenger);
+        }
     }
 }
