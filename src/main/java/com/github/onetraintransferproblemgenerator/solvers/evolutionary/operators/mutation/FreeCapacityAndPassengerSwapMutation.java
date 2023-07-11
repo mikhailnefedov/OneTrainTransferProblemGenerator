1af01afee6da06d14599e5b93643876b718df6e2
mnefedov <mikhail.nefedov@stud.htwk-leipzig.de>
2023-07-11 18:06:00 +0200

    Add free capacity and passenger swap mutation

--- src/main/java/com/github/onetraintransferproblemgenerator/solvers/evolutionary/operators/mutation/FreeCapacityAndPassengerSwapMutation.java
+++ src/main/java/com/github/onetraintransferproblemgenerator/solvers/evolutionary/operators/mutation/FreeCapacityAndPassengerSwapMutation.java
@@ -3,19 +3,25 @@ package com.github.onetraintransferproblemgenerator.solvers.evolutionary.operato
 import com.github.onetraintransferproblemgenerator.models.Passenger;
 import com.github.onetraintransferproblemgenerator.solvers.evolutionary.models.Individual;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class FreeCapacitySwapMutation implements Mutation {
+public class FreeCapacityAndPassengerSwapMutation implements Mutation {
 
     private Random random;
 
+    @Override
     public void mutate(Individual individual) {
         random = new Random();
         if (individual.getPassengerRailCarriageMapping().keySet().size() > 0) {
             Passenger passenger = chooseRandomPassenger(individual);
 
-            List<? extends Swap> swaps = SwapCreator.createSwapsWithFreeCapacity(individual, passenger);
+            List<? extends Swap> freeCapacitySwaps = SwapCreator.createSwapsWithFreeCapacity(individual, passenger);
+            List<? extends Swap> passengerSwaps = SwapCreator.createPassengerSwaps(individual, passenger);
+            List<Swap> swaps = new ArrayList<>();
+            swaps.addAll(freeCapacitySwaps);
+            swaps.addAll(passengerSwaps);
             if (swaps.size() > 0) {
                 Swap swap = swaps.get(random.nextInt(swaps.size()));
                 swap.makeSwap(individual, passenger);
@@ -28,5 +34,4 @@ public class FreeCapacitySwapMutation implements Mutation {
 
         return passengers.get(random.nextInt(passengers.size()));
     }
-
 }
