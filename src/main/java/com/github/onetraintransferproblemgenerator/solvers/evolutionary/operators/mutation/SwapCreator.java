d9481c060a097d806483d8ac4d51fc45718a0a35
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2023-07-22 19:09:12 +0200

    Rework passenger swap mutation
    Too many resources are needed, should not be used

--- src/main/java/com/github/onetraintransferproblemgenerator/solvers/evolutionary/operators/mutation/SwapCreator.java
+++ src/main/java/com/github/onetraintransferproblemgenerator/solvers/evolutionary/operators/mutation/SwapCreator.java
@@ -1,11 +1,10 @@
 package com.github.onetraintransferproblemgenerator.solvers.evolutionary.operators.mutation;
 
 import com.github.onetraintransferproblemgenerator.models.Passenger;
-import com.github.onetraintransferproblemgenerator.solvers.evolutionary.RailCarriageSectionSet;
 import com.github.onetraintransferproblemgenerator.solvers.evolutionary.models.Individual;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,21 +14,23 @@ public class SwapCreator {
     public static List<FreeCapacitySwap> createSwapsWithFreeCapacity(Individual individual, Passenger passenger) {
         int currentRailCarriageId = individual.getPassengerRailCarriageMapping().get(passenger);
         return individual.railCarriagesWithCapacityForRide(passenger.getInStation(), passenger.getOutStation()).stream()
-                .filter(id -> !id.equals(currentRailCarriageId))
-                .map(FreeCapacitySwap::new)
-                .toList();
+            .filter(id -> !id.equals(currentRailCarriageId))
+            .map(FreeCapacitySwap::new)
+            .toList();
     }
 
     public static List<PassengerSwap> createPassengerSwaps(Individual individual, Passenger passenger) {
-        Map<Integer, List<Passenger>> possiblePassengerSwaps = getPossiblePassengerSwapPartners(individual, passenger);
+        Map<Integer, List<List<Passenger>>> possiblePassengerSwaps = getPossiblePassengerSwapPartners(individual, passenger);
+        List<PassengerSwap> passengerSwaps = new ArrayList<>();
 
-        return possiblePassengerSwaps.entrySet().stream().map(entry -> {
-            List<Passenger> swapPassengers = entry.getValue();
-            return new PassengerSwap(entry.getKey(), swapPassengers);
-        }).toList();
+        possiblePassengerSwaps.forEach((key, value) ->
+            value.forEach(passengerList -> passengerSwaps.add(new PassengerSwap(key, passengerList)))
+        );
+
+        return passengerSwaps;
     }
 
-    private static Map<Integer, List<Passenger>> getPossiblePassengerSwapPartners(Individual individual, Passenger passenger) {
+    private static Map<Integer, List<List<Passenger>>> getPossiblePassengerSwapPartners(Individual individual, Passenger passenger) {
         int railCarriageId = individual.getRailCarriageIdOfPassenger(passenger);
 
         Map<Integer, List<Passenger>> possibleSwapPassengers = individual.getPassengerRailCarriageMapping().keySet().stream()
@@ -43,9 +44,9 @@ public class SwapCreator {
 
         });
 
-        return null;
-
-        //TODO: FAULT, DOES NOT WORK CORRECTLY, CHANGE NEEDED
+        return possibleSwapPassengers.entrySet().stream()
+            .collect(Collectors.toMap(entry -> entry.getKey(),
+                entry -> getPassengerSwapCombinationsOfRailCarriage(individual, entry.getKey(), entry.getValue(), passenger.getInStation(), passenger.getOutStation() - 1)));
     }
 
     private static List<List<Passenger>> getPassengerSwapCombinationsOfRailCarriage(Individual individual,
@@ -53,41 +54,77 @@ public class SwapCreator {
                                                                                     List<Passenger> passengers,
                                                                                     int inStation,
                                                                                     int penultimateStation) {
-        List<Integer> inStationsWithFreeCapacity = getInStationsWithFreeCapacity(individual, railCarriageId);
+        List<Integer> inStationsWithFreeCapacity = getInStationsWithFreeCapacity(individual, inStation, penultimateStation, railCarriageId);
 
         Map<Integer, List<Passenger>> passengersOfInStation = passengers.stream().collect(Collectors.groupingBy(Passenger::getInStation));
-
+        for (int i = inStation; i <= penultimateStation; i++) {
+            if (!passengersOfInStation.containsKey(i)) {
+                passengersOfInStation.put(i, new ArrayList<>());
+            }
+        }
 
         List<List<Passenger>> combinations = new ArrayList<>();
         for (int stationId = inStation; stationId <= penultimateStation; stationId++) {
             if (combinations.size() > 0) {
+                List<List<Passenger>> newCombinations = new ArrayList<>();
                 for (List<Passenger> combination : combinations) {
-                    if (combination.get(combination.size() - 1).equals(null)) {
+                    if (combination.get(combination.size() - 1) == null) {
                         if (inStationsWithFreeCapacity.contains(stationId)) {
-                            combination.add(null);
-                        } else {
-
+                            List<Passenger> newCombination = new ArrayList<>(combination);
+                            newCombination.add(null);
+                            newCombinations.add(newCombination);
+                        }
+                        List<Passenger> laterPassengers = passengersOfInStation.get(stationId);
+                        for (Passenger p : laterPassengers) {
+                            List<Passenger> newCombination = new ArrayList<>(combination);
+                            newCombination.add(p);
+                            newCombinations.add(newCombination);
+                        }
+                    } else if (combination.get(combination.size() - 1).getOutStation() == stationId) {
+                        if (inStationsWithFreeCapacity.contains(stationId)) {
+                            List<Passenger> newCombination = new ArrayList<>(combination);
+                            newCombination.add(null);
+                            newCombinations.add(newCombination);
+                        }
+                        List<Passenger> laterPassengers = passengersOfInStation.get(stationId);
+                        for (Passenger p : laterPassengers) {
+                            List<Passenger> newCombination = new ArrayList<>(combination);
+                            newCombination.add(p);
+                            newCombinations.add(newCombination);
                         }
                     }
-
                 }
+                combinations = newCombinations;
             } else {
                 if (inStationsWithFreeCapacity.contains(stationId)) {
-                    combinations.add(new ArrayList<>(null));
+                    List<Passenger> newCombination = new ArrayList<>();
+                    newCombination.add(null);
+                    combinations.add(newCombination);
                 }
-                //List<Passenger>
+                List<Passenger> laterPassengers = passengersOfInStation.get(stationId);
+                for (Passenger p : laterPassengers) {
+                    List<Passenger> newCombination = new ArrayList<>();
+                    newCombination.add(p);
+                    combinations.add(newCombination);
+                }
             }
         }
-
-
-        return null;
+        cleanPassengerCombinations(combinations);
+        return combinations;
     }
 
-    private static List<Integer> getInStationsWithFreeCapacity(Individual individual, int railCarriageId) {
-        return individual.getPassengersOfRailCarriageOfInStation().get(railCarriageId).entrySet().stream()
-            .filter(entry -> entry.getValue().hasFreeCapacity())
+    private static List<Integer> getInStationsWithFreeCapacity(Individual individual, int inStation,
+                                                               int penultimateStation, int railCarriageId) {
+
+        return individual.getPassengersOfRailCarriageOfInStation().entrySet().stream()
+            .filter(entry -> entry.getKey() >= inStation && entry.getKey() <= penultimateStation)
+            .filter(entry -> entry.getValue().get(railCarriageId).hasFreeCapacity())
             .map(Map.Entry::getKey)
             .collect(Collectors.toList());
+    }
+
+    private static void cleanPassengerCombinations(List<List<Passenger>> passengerCombinations) {
+        passengerCombinations.forEach(passengers -> passengers.removeAll(Collections.singleton(null)));
     }
 
 }
