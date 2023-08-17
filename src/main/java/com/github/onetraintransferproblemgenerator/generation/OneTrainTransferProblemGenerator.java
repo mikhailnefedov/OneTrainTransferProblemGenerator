a95397be5a8aa3fad378cfaacb551ae88a2e67bb
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2023-08-17 10:57:09 +0200

    Fix not solving every instance

--- src/main/java/com/github/onetraintransferproblemgenerator/generation/OneTrainTransferProblemGenerator.java
+++ src/main/java/com/github/onetraintransferproblemgenerator/generation/OneTrainTransferProblemGenerator.java
@@ -5,18 +5,18 @@ import com.github.onetraintransferproblemgenerator.models.OneTrainTransferProble
 public interface OneTrainTransferProblemGenerator {
 
     int MIN_STATION_COUNT = 3;
-    int MAX_STATION_COUNT = 10;
+    int MAX_STATION_COUNT = 6;
 
     double MIN_CONGESTION = 0.05;
-    double MAX_CONGESTION = 1.0;
+    double MAX_CONGESTION = 1.05;
     double CONGESTION_INCREMENT = 0.05;
 
     /**
      * Configuration for SimpleGenerator
      */
     int MIN_RAIL_CARRIAGES = 1;
-    int MAX_RAIL_CARRIAGES = 8;
-    int MAX_RAIL_CARRIAGE_CAPACITY = 100;
+    int MAX_RAIL_CARRIAGES = 4;
+    int MAX_RAIL_CARRIAGE_CAPACITY = 20;
     int POSITION_COUNT = 14;
 
     OneTrainTransferProblem generate();
